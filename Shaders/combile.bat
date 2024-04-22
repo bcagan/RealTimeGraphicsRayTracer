e3ddf888e1109caad882ff7c1a56251566a092c6
Ben Cagan <30479930+bcagan@users.noreply.github.com>
2024-04-22 05:31:01 -0400

    refactor: updates rt shader names
    RT shaders have diverged enough from nvpro examples to justify changing name

--- Shaders/combile.bat
+++ Shaders/combile.bat
@@ -10,7 +10,7 @@ C:/VulkanSDK/1.3.268.0/Bin/glslc.exe shader.frag -o frag.spv
 C:/VulkanSDK/1.3.268.0/Bin/glslc.exe shaderEnv.frag -o fragEnv.spv
 C:/VulkanSDK/1.3.268.0/Bin/glslc.exe fragFinal.frag -o fragFinal.spv
 C:/VulkanSDK/1.3.268.0/Bin/glslc.exe rtFinal.frag -o rtFinal.spv
-C:/VulkanSDK/1.3.268.0/Bin/glslc.exe --target-spv=spv1.6 raytraceEXAMPLE.rgen -o rayGen.spv
-C:/VulkanSDK/1.3.268.0/Bin/glslc.exe --target-spv=spv1.6 raytraceEXAMPLE.rmiss -o miss.spv
-C:/VulkanSDK/1.3.268.0/Bin/glslc.exe --target-spv=spv1.6 raytraceEXAMPLE.rchit -o closestHit.spv
+C:/VulkanSDK/1.3.268.0/Bin/glslc.exe --target-spv=spv1.6 raytrace.rgen -o rayGen.spv
+C:/VulkanSDK/1.3.268.0/Bin/glslc.exe --target-spv=spv1.6 raytrace.rmiss -o miss.spv
+C:/VulkanSDK/1.3.268.0/Bin/glslc.exe --target-spv=spv1.6 raytrace.rchit -o closestHit.spv
 pause
